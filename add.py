ce98afe9fe32c1f5352f6701a645033ae607c392
apolo51 <123968352+apolo51@users.noreply.github.com>
2023-02-01 16:30:33 +0000

    kk

--- add.py
+++ add.py
@@ -1,7 +1,10 @@
 import requests
 import time
+import json
 
-head = {'authority': 'api.faucetpay.io', 'accept': 'application/json, text/plain, */*', 'accept-language': 'en-US,en;q=0.9,id;q=0.8', 'authorization': 'Bearer 3b0860ea9cf70809919281466257689cfef97c1fd24e1d771b7f415e64feab84', 'content-type': 'application/json', 'origin': 'https://faucetpay.io', 'referer': 'https://faucetpay.io/', 'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Mobile Safari/537.36'}
+authb = input('AUTH KEY : ')
+
+head = {'authority': 'api.faucetpay.io', 'accept': 'application/json, text/plain, */*', 'accept-language': 'en-US,en;q=0.9,id;q=0.8', 'authorization': str(authb), 'content-type': 'application/json', 'origin': 'https://faucetpay.io', 'referer': 'https://faucetpay.io/', 'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Mobile Safari/537.36'}
 
 file = open("trxAdd.txt", mode='r')
 
@@ -9,10 +12,20 @@ conten = file.read()
 
 wl = conten.split("\n")
 
-def runer(walet, x):
+def runer(walet, x): 
 	r = requests.post('https://api.faucetpay.io/wallet/link-address', headers=head, json={"coin": "TRX", "label": "TRONGG " + str(x), "address": str(walet)})
-	print(r.text)
+	return r.text
 
-x = 0
+x = 499
 
-runer(wl[x], x)
+while x < 8000:
+	coba = runer(wl[x], x)
+	print(coba)
+	if coba == '{"success":false,"message":"You have reached the maximum number of linked addresses."}':
+		x = 8001
+	if coba == '{"success":true,"message":"Great news! The address has been linked to your account."}' or '{"success":false,"message":"This address is already linked to your account."}':
+		x+=1
+		time.sleep(1)
+	else:
+		print('Wait 5 Sec')
+		time.sleep(5)
